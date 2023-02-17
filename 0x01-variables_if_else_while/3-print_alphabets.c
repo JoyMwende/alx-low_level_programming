e2678e0d2bc0d73597532f3c86adb2511cf4c596
JoyMwende <joymwendekairi@gmail.com>
2023-02-17 01:33:14 -0800

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,18 +10,18 @@ int main(void)
 	char lower = 'a';
 	char upper = 'A';
 
-	while (lower <= 'z') /*Print lowercase a-z*/
+	while (lower <= 'z') /*print lowercases a-z*/
 	{
 		putchar(lower);
 		lower++;
 	}
 
-	while (upper <= 'Z') /*Print uppercase A-Z*/
-        {
-                putchar(upper);
-                upper++;
-        }
-	
+	while (upper <= 'Z') /*print uppercase A-Z*/
+	{
+		putchar(upper);
+		upper++;
+	}
+
 	putchar('\n');
 
 	return (0);
