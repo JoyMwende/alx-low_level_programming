5719edc8716342bd5cbc5c467af2b4d7eaa83ac2
JoyMwende <joymwendekairi@gmail.com>
2023-04-07 06:33:20 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	s = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+	s = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (s == -1)
 		return (-1);
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	if (n = 0; text_content[n]; n++)
+	for (n = 0; text_content[n]; n++)
 		;
 
 	r = write(s, text_content, n);
